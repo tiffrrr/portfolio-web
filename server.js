ca39bfe5c069a35232dac97f6bcd46ebf60d9981
tiffrrr <53456154+tiffrrr@users.noreply.github.com>
2020-01-15 15:48:11 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');  
+http.createServer(function(req,res){   
+    if(req.url == "/"){ 
+        //res 送出回應
+        res.writeHead(200,{'Content-Type':'text/html'});  
+        res.write("nodeJS hello world");
+        res.end()
+    } else if(req.url=='/test'){   //輸入http://localhost:8080/test看到的
+        res.writeHead(200,{'Content-Type':'text/html'});  
+        res.end('nodeJS test page ')
+    }
+}).listen(8080);  
