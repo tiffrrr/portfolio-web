5997f61b1115931e1dc0a15bc67421345d818885
tiffrrr <tiffanyrong20@gmail.com>
2020-02-17 16:37:08 +0800

    add server.js

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-var http = require('http');  
-http.createServer(function(req,res){   
-    if(req.url == "/"){ 
-        //res 送出回應
-        res.writeHead(200,{'Content-Type':'text/html'});  
-        res.write("nodeJS hello world");
-        res.end()
-    } else if(req.url=='/test'){   //輸入http://localhost:8080/test看到的
-        res.writeHead(200,{'Content-Type':'text/html'});  
-        res.end('nodeJS test page ')
-    }
-}).listen(8080);  
-console.log('Server running ');  
+var express = require("express"),
+    app = express(),
+    port = 3080;
+
+
+app.use('/funplay',express.static( __dirname )) //__dirname表示server.js所在的當前目錄
+
+app.listen(port, function(err) {
+    console.log("running server on from port:::::::" + port);
+});
